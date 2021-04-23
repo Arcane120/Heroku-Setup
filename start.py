bda7dc6e0ff2859e2a36d8707794855f09b3e435
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-04-23 11:55:32 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja bhai
-system ("git clone https://GitHub.com/LEGENDXOP/MASTERBOT && cd MASTERBOT && python3 -m ULTRA")
+system ("git clone https://GitHub.com/Arcane120/ARCANE-USERBOT && cd ARCANE-USERBOT && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
+system("python3 -m userbot")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
